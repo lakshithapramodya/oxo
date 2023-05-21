f5dae214cc5997d360655ce3513b834bd9bf3260
Lakshitha <pramodya8@gmail.com>
2023-05-21 10:17:31 +0530

    bug fixed

--- .history/src/components/bottomSection/FeaturesCard_20230521101441.jsx
+++ .history/src/components/bottomSection/FeaturesCard_20230521101441.jsx
@@ -4,11 +4,11 @@ export default function FeaturesCard({ title, description, icon, iconBg }) {
   return (
     <div className="bg-white flex items-start justify-between p-10 rounded-xl pl-4 xl:pr-12 border col-span-1">
       <div
-        className={`w-20 h-20 sm:w-24 sm:h-24 rounded-full ${iconBg} flex items-center justify-center`}
+        className={`w-24 h-24 rounded-full ${iconBg} flex items-center justify-center`}
       >
         {icon}
       </div>
-      <div className="w-[70%] ">
+      <div className="sm:w-[70%] w-[80%] ">
         <h3 className="text-[#192864] text-xl font-semibold">{title}</h3>
         <p className="text-gray-400 mt-4 text-justify">{description}</p>
       </div>
